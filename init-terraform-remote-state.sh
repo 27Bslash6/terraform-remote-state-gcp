ff1a2d53ca1270952128c1cf64ea556b6cbdbce8
Ray Walker <hello@raywalker.it>
2017-08-16 16:09:40 +1000

    Allow specifying backend filename

--- init-terraform-remote-state.sh
+++ init-terraform-remote-state.sh
@@ -4,14 +4,18 @@ GCP_PROJECT=${GCP_PROJECT:-"$(gcloud config get-value project)"}
 GS_BUCKET=${GS_BUCKET:-"${GCP_PROJECT}-terraform-admin"}
 GS_BUCKET_PATH=${GS_BUCKET_PATH:-"/"}
 TF_CREDS=${TERRAFORM_CREDENTIALS:-"~/.config/gcloud/terraform-admin.json"}
+
 [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]] && export GOOGLE_APPLICATION_CREDENTIALS=${TF_CREDS}
+
+BACKEND_FILE=${1:-"./backend.tf"}
+echo "Using file: $BACKEND_FILE"
 
 function init_bucket {
   gsutil mb -p ${GCP_PROJECT} gs://${GCP_PROJECT}-terraform-admin
 }
 
 function write_backend_file {
-  cat > backend.tf <<EOF
+  cat > ${BACKEND_FILE} <<EOF
 terraform {
   backend "gcs" {
     bucket = "${GS_BUCKET}"
