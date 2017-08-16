427d56eacc6957f3add9c9868b4a167a7819df8e
Ray Walker <hello@raywalker.it>
2017-08-16 14:29:53 +1000

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+#Storing Terraform remote state on a Google Storage bucket for GCP applications
+
+Run the provided `init-terraform-remote-state.sh` file, and configure according to environment variables such as:
+
+`GCP_PROJECT`
+
+`GS_BUCKET`
+
+`GS_BUCKET_PATH`
+
+`TERRAFORM_CREDENTIALS`
+
+`GOOGLE_APPLICATION_CREDENTIALS`
---
+++ init-terraform-remote-state.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+GCP_PROJECT=${GCP_PROJECT:-"$(gcloud config get-value project)"}
+GS_BUCKET=${GS_BUCKET:-"${GCP_PROJECT}-terraform-admin"}
+GS_BUCKET_PATH=${GS_BUCKET_PATH:-"/"}
+TF_CREDS=${TERRAFORM_CREDENTIALS:-"~/.config/gcloud/terraform-admin.json"}
+[[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]] && export GOOGLE_APPLICATION_CREDENTIALS=${TF_CREDS}
+
+function init_bucket {
+  gsutil mb -p ${GCP_PROJECT} gs://${GCP_PROJECT}-terraform-admin
+}
+
+function write_backend_file {
+  cat > backend.tf <<EOF
+terraform {
+  backend "gcs" {
+    bucket = "${GS_BUCKET}"
+    path   = "${GS_BUCKET_PATH}"
+  }
+}
+EOF
+}
+
+init_bucket
+
+([[ -e "backend.tf" ]] && echo "Warning: not overwriting existing 'backend.tf'") || write_backend_file
+
+terraform init
